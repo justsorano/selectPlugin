0fb6db8308274d7d7e13902e955d09bf35752f77
sorano <rokudo.ananas.mukuro@gmail.com>
2021-10-05 17:08:58 +0300

    основной select закончен

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,16 +2,15 @@ import '../scss/style.scss'
 import { Select } from './select/select'
 const $ = {}
 window.$ = $
-
-const select = new Select({
+$.select = new Select({
    placeholder:'Выберете елемент',
    data:[
    {id:1,name:'Яблоко'},
-   {id:1,name:'Ананас'},
-   {id:1,name:'Грейпфрут'},
-   {id:1,name:'Манго'},
-   {id:1,name:'Апельсин'},
-   {id:1,name:'Кокос'},
+   {id:2,name:'Ананас'},
+   {id:3,name:'Грейпфрут'},
+   {id:4,name:'Манго'},
+   {id:5,name:'Апельсин'},
+   {id:6,name:'Кокос'},
 ],
    selectedId: 4,
 })
--- src/assets/js/select/select.js
+++ src/assets/js/select/select.js
@@ -1,14 +1,21 @@
-
-
-function _getTemplate(placeholder,data){
-   return  `
+function _getTemplate(data = [],placeholder,selectedId){
+   let placeholderText = placeholder ?? 'Выберете елемент'
+   const items = data.map(item =>{
+      let cls = ''
+      if(item.id === selectedId){
+         cls = 'selected'
+         placeholderText = item.name
+      }
+      return `<li class='select__item ${cls}' data-type='item' data-id='${item.id}' '>${item.name}</li>`
+   })
+   return `
    <div class='select__input' data-type='toggle'>
-      <div class='select__placeholder'>${placeholder}</div>
-      <i class="fas fa-chevron-down"></i>
+      <div class='select__placeholder' data-type='toggle' data-value='name'>${placeholderText}</div>
+      <i class='fas fa-chevron-down' data-type='toggle'></i>
    </div>
-   <div class='select__dropdown'>
-   ${data ? data.map(Select.toHtml).join('') : ''}
-   </div>
+   <ul class='select__dropdown'>
+      ${items.join('')}
+   </ul>
 `
 }
 
@@ -16,28 +23,28 @@ export class Select {
    select = document.getElementById('select')
    closing = false
    constructor(options){
+      this.selectedId = options.selectedId
       this.placeholder = options.placeholder
       this.data = options.data
       this.#render()
       this.#setap()
    }
-
    #render(){
    select.classList.add('select')
-   select.innerHTML = _getTemplate(this.placeholder,this.data)
+   select.innerHTML = _getTemplate(this.data,this.placeholder,this.selectedId)
    }
 
    #setap(){
       this.clickHandler = this.clickHandler.bind(this)
       this.select.addEventListener('click',this.clickHandler)
+      this.value = this.select.querySelector('[data-value="name"]')
    }
-
-   static toHtml(element){
-      return `
-         <div class='select__item'>${element.name}</div>
-      `
+   get isOpen(){
+      return this.select.classList.contains('open')
    }
-
+   get current(){
+      return this.data.find(item => item.id === this.selectedId)
+   }
    Open(){
       this.select.querySelector('.fa-chevron-down').style.transform = 'rotate(180deg)'
       this.select.classList.add('open')
@@ -46,14 +53,27 @@ export class Select {
       this.select.querySelector('.fa-chevron-down').style.transform = 'rotate(0)'
       this.select.classList.remove('open')
    }
-   Toggle(value){
-      value ? this.Close() : this.Open()
+   Destroy(){
+      this.select.removeEventListener('click',this.clickHandler)
+      this.select.parentNode.querySelector('.select').remove()
+   }
+   Toggle(){
+      this.isOpen ? this.Close() : this.Open()
+   }
+   Select(id){
+      this.selectedId = +id
+      this.value.textContent = this.current.name
+      this.select.querySelectorAll('[data-type="item"]').forEach(item => item.classList.remove('selected'))
+      this.select.querySelector(`[data-id="${id}"]`).classList.add('selected')
+      this.Close()
    }
    clickHandler(e){
       const type = e.target.dataset.type
       if(type === 'toggle'){
-         this.Toggle(this.closing)
-         this.closing = !this.closing
+         this.Toggle()
+      } else if(type === 'item'){
+         const id = e.target.dataset.id
+         this.Select(id)
       }
    }
 }
